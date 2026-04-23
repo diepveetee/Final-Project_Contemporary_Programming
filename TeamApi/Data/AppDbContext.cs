7ad98b14e766f377acdb7c105497a778c7f21fcb
Boehmgm <boehmgm@mail.uc.edu>
2026-04-23 16:58:05 -0400

    Added Data Seeding to provide Sample data for each table creating four pieces of data in each table.

--- TeamApi/Data/AppDbContext.cs
+++ TeamApi/Data/AppDbContext.cs
@@ -17,5 +17,151 @@ namespace TeamApi.Data
         public DbSet<Hobby> Hobbies { get; set; }
         public DbSet<FavoriteFood> FavoriteFoods { get; set; }
         public DbSet<FavoriteVideoGame> FavoriteVideoGames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeamMember>().HasData(
+                new TeamMember
+                {
+                    Id = 1,
+                    FullName = "Gabriel Boehm",
+                    BirthDate = new DateTime(2004, 10, 21),
+                    CollegeProgram = "Information Technology, Systems Adminstration and Game Design",
+                    YearInProgram = "Third Year, Junior"
+                }, 
+                new TeamMember
+                {
+                    Id = 2,
+                    FullName = "Van Diep",
+                    BirthDate = new DateTime(2001, 01, 30),
+                    CollegeProgram = "IT Software Development",
+                    YearInProgram = "Second Year, Sophmore"
+                },
+                new TeamMember
+                {
+                    Id = 3,
+                    FullName = "Jane Doe",
+                    BirthDate = new DateTime(2002, 03, 20),
+                    CollegeProgram = "IT Software Development",
+                    YearInProgram = "First Year, Freshman"
+                },
+                new TeamMember
+                {
+                    Id = 4,
+                    FullName = "Adrian Taveras",
+                    BirthDate = new DateTime(2005, 01, 04),
+                    CollegeProgram = "Trade School",
+                    YearInProgram = "Second Year, Sophmore"
+                }
+                );
+            modelBuilder.Entity<FavoriteVideoGame>().HasData(
+                new FavoriteVideoGame
+                {
+                    Id = 1,
+                    Title = "Baldurs Gate 3",
+                    Genre = "Turn-Based RPG",
+                    Platform = "Desktop",
+                    HoursPlayed = 895
+                },
+                new FavoriteVideoGame
+                {
+                    Id = 2,
+                    Title = "Rollercoaster Tycoon 2",
+                    Genre = "Strategy",
+                    Platform = "Desktop",
+                    HoursPlayed = 700
+                },
+                new FavoriteVideoGame
+                {
+                    Id = 3,
+                    Title = "Sol cesto",
+                    Genre = "Dungeon Crawler, Roguelite",
+                    Platform = "Desktop",
+                    HoursPlayed = 2
+                },
+                new FavoriteVideoGame
+                {
+                    Id = 4,
+                    Title = "MechWarrior Online",
+                    Genre = "FPS",
+                    Platform = "Desktop",
+                    HoursPlayed = 1153
+                });
+                
+            modelBuilder.Entity<FavoriteFood>().HasData(
+                new FavoriteFood
+                {
+                    Id = 1,
+                    FoodName = "Fried Chicken",
+                    ContinentOrigin = "North America",
+                    IsHot = true,
+                    IsVegetarian = false
+
+                },
+                new FavoriteFood
+                {
+                    Id = 2,
+                    FoodName = "Yogurt",
+                    ContinentOrigin = "Asia",
+                    IsHot = false,
+                    IsVegetarian = true
+
+                },
+                new FavoriteFood
+                {
+                    Id = 3,
+                    FoodName = "CheeseBurger",
+                    ContinentOrigin = "North America",
+                    IsHot = true,
+                    IsVegetarian = false
+
+                },
+                new FavoriteFood
+                {
+                    Id = 4,
+                    FoodName = "Fettaccine Alfredo",
+                    ContinentOrigin = "Europe",
+                    IsHot = true,
+                    IsVegetarian = false
+
+                });
+            modelBuilder.Entity<Hobby>().HasData(
+                new Hobby
+                {
+                    Id = 1,
+                    Name = "Video Games",
+                    Description = "Playing video games with friends.",
+                    YearsDoingIt = 16,
+                    WeeklyHours = 14
+
+                },
+                new Hobby
+                {
+                    Id = 2,
+                    Name = "Weightlifting",
+                    Description = "Lifting weights",
+                    YearsDoingIt = 6,
+                    WeeklyHours = 5
+
+                },
+                new Hobby
+                {
+                    Id = 3,
+                    Name = "Manga",
+                    Description = "Reading Manga",
+                    YearsDoingIt = 4,
+                    WeeklyHours = 24
+
+                },
+                 new Hobby
+                 {
+                     Id = 4,
+                     Name = "Cooking",
+                     Description = "Cooking meals for myself and others.",
+                     YearsDoingIt = 8,
+                     WeeklyHours = 9
+
+                 });
+        }
     }
 }
---
+++ TeamApi/Migrations/20260423205355_SeedInitialCreate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TeamApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FavoriteFoods",
+                columns: new[] { "Id", "ContinentOrigin", "FoodName", "IsHot", "IsVegetarian" },
+                values: new object[,]
+                {
+                    { 1, "North America", "Fried Chicken", true, false },
+                    { 2, "Asia", "Yogurt", false, true },
+                    { 3, "North America", "CheeseBurger", true, false },
+                    { 4, "Europe", "Fettaccine Alfredo", true, false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FavoriteVideoGames",
+                columns: new[] { "Id", "Genre", "HoursPlayed", "Platform", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Turn-Based RPG", 895, "Desktop", "Baldurs Gate 3" },
+                    { 2, "Strategy", 700, "Desktop", "Rollercoaster Tycoon 2" },
+                    { 3, "Dungeon Crawler, Roguelite", 2, "Desktop", "Sol cesto" },
+                    { 4, "FPS", 1153, "Desktop", "MechWarrior Online" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hobbies",
+                columns: new[] { "Id", "Description", "Name", "WeeklyHours", "YearsDoingIt" },
+                values: new object[,]
+                {
+                    { 1, "Playing video games with friends.", "Video Games", 14, 16 },
+                    { 2, "Lifting weights", "Weightlifting", 5, 6 },
+                    { 3, "Reading Manga", "Manga", 24, 4 },
+                    { 4, "Cooking meals for myself and others.", "Cooking", 9, 8 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TeamMembers",
+                columns: new[] { "Id", "BirthDate", "CollegeProgram", "FullName", "YearInProgram" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2004, 10, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Information Technology, Systems Adminstration and Game Design", "Gabriel Boehm", "Third Year, Junior" },
+                    { 2, new DateTime(2001, 1, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "IT Software Development", "Van Diep", "Second Year, Sophmore" },
+                    { 3, new DateTime(2002, 3, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "IT Software Development", "Jane Doe", "First Year, Freshman" },
+                    { 4, new DateTime(2005, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Trade School", "Adrian Taveras", "Second Year, Sophmore" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FavoriteFoods",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteFoods",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteFoods",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteFoods",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteVideoGames",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteVideoGames",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteVideoGames",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "FavoriteVideoGames",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hobbies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hobbies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hobbies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hobbies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMembers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMembers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMembers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMembers",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
